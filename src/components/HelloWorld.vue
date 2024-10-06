5e596eee3c2a051f1fe711115f33220e1e95b95f
EhylinPhicus <ehylin@phicus.es>
2024-10-06 17:48:15 +0200

    add vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,9 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>Hola mundo</h3>
+    <v-container>
+      <v-btn color="primary">Click Me</v-btn>
+    </v-container>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+//Vuetify
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
